55c2aece4abb132dad4d84e4a0742e88a6564ead
Pascal <pacolos@web.de>
2019-12-16 02:54:20 +0100

    Chat send option + server connection + Array recieved

--- src/main/java/org/nicolos/ChatController.java
+++ src/main/java/org/nicolos/ChatController.java
@@ -1,5 +1,6 @@
 package org.nicolos;
 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,10 +8,16 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.nicolos.client.Client;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class ChatController {
       private int uid;
@@ -19,18 +26,31 @@ public class ChatController {
 
       @FXML
       private Label lbl_username;
+      @FXML
+      private TextField txt_message;
+      @FXML
+      private TextArea tb_chat;
+
       public void Init(String _username, int _uid, Client c) {
             client = c;
             username =_username;
             uid = _uid;
+
             if(client.HasSocket())
                   lbl_username.setText(username+" "+uid);
 
+            new Timer().schedule(
+                    new TimerTask() {
+                          @Override
+                          public void run() {
+                                printInTextArea();
+                          }
+                    }, 0, 3000);
       }
 
       public void clickedOnQuit(ActionEvent e)  {
            try {
-                 client.TerminateConnection(client.GetSocket());
+                 client.TerminateConnection();
                  FXMLLoader fxmlLoader = new FXMLLoader();
                  fxmlLoader.setLocation(getClass().getResource("main.fxml"));
                  Parent switchscene = fxmlLoader.load();
@@ -42,6 +62,21 @@ public class ChatController {
            }
            catch (IOException ex) {
                  ex.printStackTrace();
+           }
+      }
+      public void printInTextArea() {
+            ArrayList<String> chat = client.GetFullChat();
+            StringBuilder sb = new StringBuilder();
+
+            for (String s : chat) {
+                  sb.append(s);
             }
+      }
+
+      public void clickedOnSend(ActionEvent e) {
+            if(txt_message.getText() != null || !txt_message.getText().trim().isEmpty()) {
+                  client.RequestConnection(txt_message.getText());
+            }
+            txt_message.clear();
       }
 }
--- src/main/java/org/nicolos/client/Client.java
+++ src/main/java/org/nicolos/client/Client.java
@@ -2,51 +2,104 @@ package org.nicolos.client;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.ConnectException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.sql.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client {
       static final int port = 45126;
       String ipToConnectTo;
+      String requestString = "/#req#/";
       boolean connected;
+
+      int currentChatLength;
+
+      ArrayList<String> fullChat;
 
       Socket socket;
 
       public Client(String ip) {
             this.ipToConnectTo = ip;
+            fullChat = new ArrayList<>();
 
-            this.socket = RequestConnection("Client requesting connection..");
+            this.socket = RequestConnection(requestString);
 
-            connected = true;
-      }
+            new Thread(() -> {
+                  while (connected) {
+                        connected = PokeServer();
 
-      boolean PokeServer (Socket server) {
-            /*if (connected) {
-                  try {
-                        Scanner in = new Scanner(server.getInputStream());
-                        PrintWriter out = new PrintWriter(server.getOutputStream(), true);
-
-                        out.println("/#req#/");
-
-                        if (in.hasNext()) {
-
+                        try {
+                              Thread.sleep(3000);
+                        } catch (InterruptedException e) {
+                              e.printStackTrace();
                         }
                   }
-                  catch (IOException e) {
-                        System.err.println("Couldn't write to server's output stream.");
-                        e.printStackTrace();
+            }).start();
+      }
+
+      boolean PokeServer() {
+            this.socket = null;
+
+            try {
+                  this.socket = new Socket(this.ipToConnectTo, port);
+
+                  if (connected) {
+                        System.out.println(Thread.currentThread().getName() + ": Poking..");
+
+                        try {
+                              Scanner in = new Scanner(socket.getInputStream());
+                              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+                              out.println(requestString);
+
+                              if (in.hasNext()) {
+                                    int serverChatLength = in.nextInt();
+                                    System.out.println("Server chat length: " + serverChatLength);
+
+                                    if (currentChatLength < serverChatLength) {
+                                          out.print(true);
+
+                                          while (in.hasNext()) {
+                                                String newChatLine = in.nextLine();
+                                                fullChat.add(newChatLine);
+                                          }
+
+                                          this.currentChatLength = fullChat.size();
+                                    } else if (currentChatLength == serverChatLength) {
+                                          out.print(false);
+                                    } else {
+                                          System.out.println("Something went wrong, please try reconnecting.");
+                                          return false;
+                                    }
+
+                                    return true;
+                              }
+                        } catch (IOException e) {
+                              System.err.println("Couldn't write to server's output stream.");
+                              e.printStackTrace();
+                        }
+                  } else {
+                        System.err.println("Couldn't poke the server.");
+                        return false;
+                  }
+            } catch (ConnectException e) {
+                  System.err.println("Server is down..");
+                  return false;
+            } catch (IOException e) {
+                  e.printStackTrace();
+            } finally {
+                  try {
+                        TerminateConnection();
+                  } catch (IOException e) {
+                        e.getMessage();
                   }
             }
-            else {
-                  System.err.println("Couldn't poke the server.");
-                  return false;
-            }
-            return false;*/
 
             return false;
       }
@@ -67,6 +120,8 @@ public class Client {
                   out.println(output);
 
                   System.out.println(output);
+
+                  connected = true;
             } catch (UnknownHostException e) {
                   System.err.println("Couldn't determine the host.");
                   e.printStackTrace();
@@ -76,59 +131,45 @@ public class Client {
                   e.printStackTrace();
                   return null;
             } finally {
-                  TerminateConnection(this.socket);
+                  try {
+                        TerminateConnection();
+                  } catch (IOException e) {
+                        e.getMessage();
+                  }
             }
 
             return socket;
       }
-      public boolean TerminateConnection (Socket socket) {
+
+      public boolean TerminateConnection() throws IOException {
             try {
                   if (socket != null) {
                         this.socket.close();
                         System.out.println("Closed the client's connection..");
                         return true;
+                  } else {
+                        throw new IOException("Socket already closed.");
                   }
-                  else {
-                        System.err.println("There was no socket to close.");
-                        return false;
-                  }
-            }
-            catch (IOException e) {
+            } catch (IOException e) {
                   e.printStackTrace();
             }
 
             return false;
       }
 
-      public boolean TerminateConnectionManually (Socket socket) {
-            try {
-                  if (socket != null) {
-                        this.socket.close();
-                        System.out.println("Closed the client's connection..");
-                        connected = true;
-                        return true;
-                  }
-                  else {
-                        System.err.println("There was no socket to close.");
-                        connected = false;
-                        return false;
-                  }
-            }
-            catch (IOException e) {
-                  e.printStackTrace();
-            }
-
-            return false;
+      public ArrayList<String> GetFullChat() {
+            return this.fullChat;
       }
 
       public boolean IsConnected() {
-            return connected;
+            return this.connected;
       }
 
-      public Socket GetSocket () {
-            return this.socket;
+      public boolean HasSocket() {
+            return (this.socket != null);
       }
-      public boolean HasSocket () {
-            return this.socket != null;
+
+      public Socket GetSocket() {
+            return this.socket;
       }
 }
