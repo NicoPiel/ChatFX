0db853028a6df400ae094b02ba6e1fd0fd69a3e2
Unknown <nico.piel@hotmail.de>
2019-12-17 18:25:03 +0100

    Bugfixes

--- src/main/java/org/nicolos/client/Client.java
+++ src/main/java/org/nicolos/client/Client.java
@@ -20,69 +20,69 @@ public class Client {
       String ipToConnectTo;
       String requestString = "/#req#/";
       boolean connected;
-
+      
       int currentChatLength;
-
+      
       ArrayList<String> fullChat;
-
+      
       ScheduledExecutorService executor;
       Socket socket;
-
+      
       public Client(String ip) {
             executor = Executors.newSingleThreadScheduledExecutor();
-
+            
             this.ipToConnectTo = ip;
             fullChat = new ArrayList<>();
-
+            
             this.socket = EstablishConnection();
-
+            
             executor.scheduleWithFixedDelay(() -> connected = PokeServer(), 0, 450, TimeUnit.MILLISECONDS);
-
+            
       }
-
+      
       synchronized boolean PokeServer() {
             this.socket = null;
-
+            
             try {
                   this.socket = new Socket(this.ipToConnectTo, port);
-
+                  
                   if (connected) {
                         System.out.println(Thread.currentThread().getName() + ": Poking..");
-
+                        
                         try {
                               Scanner in = new Scanner(socket.getInputStream());
                               PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
+                              
                               out.println(requestString); // 1
-
+                              
                               if (in.hasNext()) {
                                     int serverChatLength = in.nextInt(); // 2
                                     System.out.println("Client: Server chat length: " + serverChatLength);
-
+                                    
                                     if (currentChatLength < serverChatLength) {
                                           out.println(true); // 3
                                           System.out.println("Client: needs new chat history.");
-
+                                          
                                           out.println(currentChatLength); // 4
-
+                                          
                                           BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
+                                          
                                           String newChat = input.readLine(); // 5
                                           System.out.println("Client: Writing to chat history.");
                                           System.out.println("New chat: " + newChat);
                                           fullChat.add(newChat);
-
+                                          
                                           System.out.println("Client: chat size = " + currentChatLength);
                                           System.out.println("Client full chat: " + GetFullChatString());
                                     } else if (currentChatLength == serverChatLength) {
                                           out.print(false);
                                     }
-
+                                    
                                     this.currentChatLength = fullChat.size();
-
+                                    
                                     in.close();
                                     out.close();
-
+                                    
                                     return true;
                               }
                         } catch (IOException e) {
@@ -99,34 +99,30 @@ public class Client {
             } catch (IOException e) {
                   e.printStackTrace();
             } finally {
-                  try {
-                        TerminateConnection();
-                  } catch (IOException e) {
-                        e.getMessage();
-                  }
+                  TerminateConnection();
             }
-
+            
             return false;
       }
-
+      
       public Socket EstablishConnection() {
             this.socket = null;
-
+            
             System.out.println(Thread.currentThread().getName() + ": Establishing connection..");
-
+            
             try {
                   this.socket = new Socket(this.ipToConnectTo, port);
-
+                  
                   Scanner in = new Scanner(socket.getInputStream());
                   PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
+                  
                   out.println("#1" + requestString);
-
+                  
                   if (in.hasNext()) {
                         this.currentChatLength = in.nextInt();
                         System.out.println("Client: Server chat length established at " + currentChatLength);
                   }
-
+                  
                   connected = true;
                   System.out.println(Thread.currentThread().getName() + ": Connection established.");
             } catch (UnknownHostException e) {
@@ -136,30 +132,26 @@ public class Client {
                   System.err.println("Couldn't initialize the socket.");
                   System.out.println("The server you are trying to reach is probably down.");
             } finally {
-                  try {
-                        TerminateConnection();
-                  } catch (IOException e) {
-                        e.getMessage();
-                  }
+                  TerminateConnection();
             }
-
+            
             return this.socket;
       }
-
+      
       public Socket RequestConnection(String message) {
             this.socket = null;
-
+            
             System.out.println(Thread.currentThread().getName() + ": Requesting connection..");
-
+            
             try {
                   this.socket = new Socket(this.ipToConnectTo, port);
                   Scanner in = new Scanner(this.socket.getInputStream());
                   PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
-
+                  
                   out.println(message);
-
+                  
                   System.out.println(message);
-
+                  
                   connected = true;
             } catch (UnknownHostException e) {
                   System.err.println("Couldn't determine the host.");
@@ -170,10 +162,10 @@ public class Client {
             } finally {
                   TerminateConnection();
             }
-
+            
             return this.socket;
       }
-
+      
       public void TerminateConnection() {
             try {
                   if (socket != null) {
@@ -185,43 +177,43 @@ public class Client {
             } catch (IOException e) {
                   System.err.println("Socket already closed.");
             }
-
+            
             if (!executor.isShutdown()) executor.shutdown();
       }
-
+      
       public void TerminateConnectionManually() {
             TerminateConnection();
-
+            
             connected = false;
       }
-
+      
       public String GetFullChatString() {
             StringBuilder sb = new StringBuilder();
-
+            
             for (String s : GetFullChat()) {
                   sb.append(s);
                   sb.append(", ");
             }
-
+            
             return sb.toString();
       }
-
+      
       public ArrayList<String> GetFullChat() {
             return this.fullChat;
       }
-
+      
       public boolean IsConnected() {
             return this.connected;
       }
-
+      
       public boolean HasSocket() {
             return (this.socket != null);
       }
-
+      
       public Socket GetSocket() {
             return this.socket;
       }
-
+      
       public int GetInterval() {
             return this.checkInterval;
       }
